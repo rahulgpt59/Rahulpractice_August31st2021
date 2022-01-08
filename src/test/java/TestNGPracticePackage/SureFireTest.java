c0894842019e1146520bc69e62333b856506880a
rahulgpt59 <72300181+rahulgpt59@users.noreply.github.com>
2022-01-08 16:08:28 +0530

    New test added

--- src/test/java/TestNGPracticePackage/SureFireTest.java
+++ src/test/java/TestNGPracticePackage/SureFireTest.java
@@ -9,6 +9,11 @@ public class SureFireTest {
         System.out.println("I am surefire test"+Thread.currentThread().getName());
     }
     @Test
+    public void surefireTest1()
+    {
+        System.out.println("I am surefire test1"+Thread.currentThread().getName());
+    }
+    @Test
     public void rahul()
     {
         System.out.println("I am Rahul test"+Thread.currentThread().getName());
