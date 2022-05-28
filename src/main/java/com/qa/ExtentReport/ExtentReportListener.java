18c1dbb1eb9b96ea17e9239f6456cab5c3ea3226
rahulgpt59 <rahulgpt59@gmail.com>
2022-05-28 12:44:37 +0530

    Remove unused class for TestNg Listener

--- src/main/java/com/qa/ExtentReport/ExtentReportListener.java
+++
@@ -1,9 +0,0 @@
-package com.qa.ExtentReport;
-
-import com.relevantcodes.extentreports.ExtentReports;
-import groovyjarjarantlr.debug.InputBufferReporter;
-
-public class ExtentReportListener  {
-
-    //private ExtentReports extent;
-}
